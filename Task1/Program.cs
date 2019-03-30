6a7a3315c19d2de7f8a2ff0def88d33a99724a3b
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2019-03-30 19:30:37 +0600

    Quiz

---
+++ Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace QuizMe {
+    [Serializable]
+    public class Point {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    [Serializable]
+    public class Snake {
+        List<Point> body = new List<Point>();
+        public ConsoleColor BodyColor { get; set; }
+
+        public Snake() {
+            body.Add(new Point { X = 47, Y = 4});
+            body.Add(new Point { X = 48, Y = 4 });
+            body.Add(new Point { X = 49, Y = 4 });
+            body.Add(new Point { X = 50, Y = 4 });
+            body.Add(new Point { X = 51, Y = 4 });
+            BodyColor = ConsoleColor.Red;
+        }
+
+        public void Draw() {
+            foreach(var el in body) {
+                Console.SetCursorPosition(el.X, el.Y);
+                Console.ForegroundColor = BodyColor;
+                Console.Write("@");
+            }
+        }
+    }
+
+
+    class Program {
+        static void Main(string[] args) {
+            Console.CursorVisible = false;
+            Snake sn;
+            XmlSerializer xSer = new XmlSerializer(typeof(Snake));
+            using(FileStream fs = new FileStream(@"C:\Users\Kirill\Desktop\snake.xml", FileMode.OpenOrCreate)) {
+                sn = xSer.Deserialize(fs) as Snake;
+            }
+            sn.Draw();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2 {
+    class Program {
+        static void Main(string[] args) {
+            DateTime start = DateTime.Now;
+            TimeSpan ans = new TimeSpan();
+            List<TimeSpan> l = new List<TimeSpan>();
+            Thread t1 = new Thread(() => {
+                while(true) {
+                    ConsoleKeyInfo k = Console.ReadKey();
+                    if(k.Key == ConsoleKey.Spacebar) {
+                        l.Add(ans);
+                    }
+                    for(int i = 0; i < l.Count; i++) {
+                        Console.SetCursorPosition(0, i + 2);
+                        Console.WriteLine(l[i]);
+                    }
+                }
+            });
+            t1.Start();
+            while(true) {
+                Console.SetCursorPosition(0, 0);
+                DateTime cur = DateTime.Now;
+                ans = cur.Subtract(start);
+                Console.WriteLine(ans);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
